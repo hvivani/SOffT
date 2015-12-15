9a10722ac0bc91224a7c179d16bcfb2aba7c15ed
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2015-12-15 08:29:53 -0300

    Agregado autor cpereyra donde corresponde
    Signed-off-by: Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>

--- SOffT.Reloj/Reloj.View/frmReloj.designer.cs
+++ SOffT.Reloj/Reloj.View/frmReloj.designer.cs
@@ -1,4 +1,25 @@
-﻿namespace Reloj.View
+﻿//
+//  frmReloj.designer.cs
+//
+//  Author:
+//       Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
+//
+//  Copyright (c) 2010 Hamekoz
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Reloj.View
 {
     partial class frmReloj
     {
