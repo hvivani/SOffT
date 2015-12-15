940d17dd94a237249353b9ac2c43209e31297c6e
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2015-12-15 12:26:09 -0300

    Refactorizacion de SOffT.Utils Codigo bonito. Convenciones de escritura de codigo
    Signed-off-by: Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>

--- SOffT.Utils/Usuario.cs
+++ SOffT.Utils/Usuario.cs
@@ -1,4 +1,4 @@
-/*  
+/*
     Usuario.cs
 
     Author:
@@ -21,120 +21,134 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 
 namespace Sofft.Utils
 {
-    public class Usuario
-    {
-        private int idUsuario;
-        private string nombre;
-        private string login;
-        private string password;
-        private int idEmpleado;
-        private bool seguridad;
-        private bool documentos;
-        private bool eliminado;
+	public class Usuario
+	{
+		public int IdUsuario {
+			get;
+			set;
+		}
 
-        public Usuario() { }
-        
-        public int IdUsuario
-        {
-            get { return idUsuario; }
-            set { idUsuario = value; }
-        }
+		public string Login {
+			get;
+			set;
+		}
 
-        public string Login
-        {
-            get { return login; }
-            set { login = value; }
-        }
+		public string Password {
+			get;
+			set;
+		}
 
-        public string Password
-        {
-            get { return password; }
-            set { password = value; }
-        }
-        
-        public string Nombre
-        {
-            get { return nombre; }
-            set { nombre = value; }
-        }
+		public string Nombre {
+			get;
+			set;
+		}
 
-        public int IdEmpleado
-        {
-            get { return idEmpleado; }
-            set { idEmpleado = value; }
-        }
-        
-        public bool Seguridad
-        {
-            get { return seguridad; }
-            set { seguridad = value; }
-        }
-        
-        public bool Documentos
-        {
-            get { return documentos; }
-            set { documentos = value; }
-        }
+		public int IdEmpleado {
+			get;
+			set;
+		}
 
-        public bool Eliminado
-        {
-            get { return eliminado; }
-            set { eliminado = value; }
-        }
+		public bool Seguridad {
+			get;
+			set;
+		}
 
-        public int consultarExisteUsuario(string usr, string pwd)
-        {
-            int idUsuario;
-            idUsuario = Convert.ToInt32(Model.DB.ejecutarScalar(Model.TipoComando.SP, "usuariosConsultarSiExiste", "usr", usr, "pwd", pwd));
-            if (idUsuario > 0)
-                return idUsuario;
-            else
-                return 0;
-        }
+		public bool Documentos {
+			get;
+			set;
+		}
 
-        public static void setPermisosIndices(ref Button[] botones)
-        {
-            DataSet ds;
-            ds = Model.DB.ejecutarDataSet(Model.TipoComando.SP, "permisosUsuariosConsultarIndice", "@idUsuario", Modulo.Usuario.idUsuario, "@idModulo", Modulo.IdModulo);
-            foreach (DataRow dr in ds.Tables["permisosUsuariosConsultarIndice"].Rows)
-            {
-                botones[(int)dr["indice"] - 1 ].Enabled = (bool)dr["acceso"];
-            }
-        }
+		public bool Eliminado {
+			get;
+			set;
+		}
 
-        public static void setPermisosSubIndices(ref Button[] botones, int indice)
-        {
-            if (Modulo.Usuario != null)
-            {
-                DataSet ds;
-                ds = Model.DB.ejecutarDataSet(Model.TipoComando.SP, "permisosUsuariosConsultarSubIndice", "idUsuario", Modulo.Usuario.idUsuario, "idModulo", Modulo.IdModulo, "indice", indice);
-                foreach (DataRow dr in ds.Tables["permisosUsuariosConsultarSubIndice"].Rows)
-                {
-                    botones[(int)dr["subIndice"] - 1].Enabled = (bool)dr["acceso"];
-                }
-            }
-        }
+		[Obsolete ("Usar ExisteUsuario()")]
+		public int consultarExisteUsuario (string usr, string pwd)
+		{
+			return ExisteUsuario (usr, pwd);
+		}
 
-        public static void setPermisos(ref Button[] botones, string nivel)
-        {
-            if (Modulo.Usuario != null)
-            {
-                DataSet ds = Model.DB.ejecutarDataSet(Model.TipoComando.SP, "permisosConsultarNivel", "@idUsuario", Modulo.Usuario.idUsuario, "@idModulo", Modulo.IdModulo, "nivel", nivel);
-                foreach (DataRow dr in ds.Tables["permisosConsultarNivel"].Rows)
-                {
-                    try
-                    { botones[Convert.ToInt32(dr["indice"])].Enabled = (bool)dr["acceso"]; }
-                    catch (Exception)
-                    { System.Console.WriteLine("Eliminar Permisos del Nivel: " + nivel + " indice: " + dr["indice"].ToString() + " por no corresponder a ningun boton"); }
-                }
-            }
-        }
-    }
+		public int ExisteUsuario (string usr, string pwd)
+		{
+			int idUsuario;
+			idUsuario = Convert.ToInt32 (Model.DB.ejecutarScalar (Model.TipoComando.SP, "usuariosConsultarSiExiste", "usr", usr, "pwd", pwd));
+			return idUsuario > 0 ? idUsuario : 0;
+		}
+
+		[Obsolete ("Usar SetPermisos (ref List<Button> botones, string nivel)")]
+		public static void setPermisos (ref Button[] botones, string nivel)
+		{
+			if (Modulo.Usuario != null) {
+				var ds = Model.DB.ejecutarDataSet (Model.TipoComando.SP, "permisosConsultarNivel", "@idUsuario", Modulo.Usuario.IdUsuario, "@idModulo", Modulo.IdModulo, "nivel", nivel);
+				foreach (DataRow dr in ds.Tables["permisosConsultarNivel"].Rows) {
+					try {
+						botones [Convert.ToInt32 (dr ["indice"])].Enabled = (bool)dr ["acceso"];
+					} catch (Exception) {
+						Console.WriteLine ("Eliminar Permisos del Nivel: " + nivel + " indice: " + dr ["indice"] + " por no corresponder a ningun boton");
+					}
+				}
+			}
+		}
+
+		public static void SetPermisos (ref List<Button> botones, string nivel)
+		{
+			if (Modulo.Usuario != null) {
+				var ds = Model.DB.ejecutarDataSet (Model.TipoComando.SP, "permisosConsultarNivel", "@idUsuario", Modulo.Usuario.IdUsuario, "@idModulo", Modulo.IdModulo, "nivel", nivel);
+				foreach (DataRow dr in ds.Tables["permisosConsultarNivel"].Rows) {
+					try {
+						botones [Convert.ToInt32 (dr ["indice"])].Enabled = (bool)dr ["acceso"];
+					} catch (Exception) {
+						Console.WriteLine ("Eliminar Permisos del Nivel: " + nivel + " indice: " + dr ["indice"] + " por no corresponder a ningun boton");
+					}
+				}
+			}
+		}
+
+		[Obsolete ("Usar SetPermisosIndices (ref List<Button> botones)")]
+		public static void setPermisosIndices (ref Button[] botones)
+		{
+			DataSet ds;
+			ds = Model.DB.ejecutarDataSet (Model.TipoComando.SP, "permisosUsuariosConsultarIndice", "@idUsuario", Modulo.Usuario.IdUsuario, "@idModulo", Modulo.IdModulo);
+			foreach (DataRow dr in ds.Tables["permisosUsuariosConsultarIndice"].Rows) {
+				botones [(int)dr ["indice"] - 1].Enabled = (bool)dr ["acceso"];
+			}
+		}
+
+		public static void SetPermisosIndices (ref List<Button> botones)
+		{
+			var ds = Model.DB.ejecutarDataSet (Model.TipoComando.SP, "permisosUsuariosConsultarIndice", "@idUsuario", Modulo.Usuario.IdUsuario, "@idModulo", Modulo.IdModulo);
+			foreach (DataRow dr in ds.Tables["permisosUsuariosConsultarIndice"].Rows) {
+				botones [(int)dr ["indice"] - 1].Enabled = (bool)dr ["acceso"];
+			}
+		}
+
+		[Obsolete ("Usar SetPermisosSubIndices (ref List<Button> botones, int indice)")]
+		public static void setPermisosSubIndices (ref Button[] botones, int indice)
+		{
+			if (Modulo.Usuario != null) {
+				var ds = Model.DB.ejecutarDataSet (Model.TipoComando.SP, "permisosUsuariosConsultarSubIndice", "idUsuario", Modulo.Usuario.IdUsuario, "idModulo", Modulo.IdModulo, "indice", indice);
+				foreach (DataRow dr in ds.Tables["permisosUsuariosConsultarSubIndice"].Rows) {
+					botones [(int)dr ["subIndice"] - 1].Enabled = (bool)dr ["acceso"];
+				}
+			}
+		}
+
+		public static void SetPermisosSubIndices (ref List<Button> botones, int indice)
+		{
+			if (Modulo.Usuario != null) {
+				var ds = Model.DB.ejecutarDataSet (Model.TipoComando.SP, "permisosUsuariosConsultarSubIndice", "idUsuario", Modulo.Usuario.IdUsuario, "idModulo", Modulo.IdModulo, "indice", indice);
+				foreach (DataRow dr in ds.Tables["permisosUsuariosConsultarSubIndice"].Rows) {
+					botones [(int)dr ["subIndice"] - 1].Enabled = (bool)dr ["acceso"];
+				}
+			}
+		}
+	}
 }
--- SOffT.Utils/Varios.cs
+++ SOffT.Utils/Varios.cs
@@ -1,8 +1,9 @@
-/*  
+/*
     Varios.cs
 
     Author:
     Hernan Vivani <hernan@vivani.com.ar> - http://hvivani.com.ar
+    Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
 
     Copyright © SOffT 2010 - http://www.sofft.com.ar
 
@@ -20,169 +21,176 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 using System;
 using System.Windows.Forms;
 
 namespace Sofft.Utils
 {
-    public class Varios
-    {
+	public static class Varios
+	{
+		public enum DateInterval
+		{
+			Second,
+			Minute,
+			Hour,
+			Day,
+			Week,
+			Month,
+			Quarter,
+			Year
+		}
 
-        public enum DateInterval
-        {
-            Second, Minute, Hour, Day, Week, Month, Quarter, Year
-        }
+		public static long DateDiff (DateInterval Interval, DateTime StartDate, DateTime EndDate)
+		{
+			long lngDateDiffValue = 0;
+			var TS = new TimeSpan (EndDate.Ticks - StartDate.Ticks);
+			switch (Interval) {
+			case DateInterval.Day:
+				lngDateDiffValue = (long)TS.Days;
+				break;
+			case DateInterval.Hour:
+				lngDateDiffValue = (long)TS.TotalHours;
+				break;
+			case DateInterval.Minute:
+				lngDateDiffValue = (long)TS.TotalMinutes;
+				break;
+			case DateInterval.Month:
+				lngDateDiffValue = (long)(TS.Days / 30);
+				break;
+			case DateInterval.Quarter:
+				lngDateDiffValue = (long)((TS.Days / 30) / 3);
+				break;
+			case DateInterval.Second:
+				lngDateDiffValue = (long)TS.TotalSeconds;
+				break;
+			case DateInterval.Week:
+				lngDateDiffValue = (long)(TS.Days / 7);
+				break;
+			case DateInterval.Year:
+				lngDateDiffValue = (long)(TS.Days / 365);
+				break;
+			}
+			return (lngDateDiffValue);
+		}
+		//end of DateDiff
 
-        public static long DateDiff(DateInterval Interval, System.DateTime StartDate, System.DateTime EndDate)
-        {
-            long lngDateDiffValue = 0;
-            System.TimeSpan TS = new System.TimeSpan(EndDate.Ticks - StartDate.Ticks);
-            switch (Interval)
-            {
-                case DateInterval.Day:
-                    lngDateDiffValue = (long)TS.Days;
-                    break;
-                case DateInterval.Hour:
-                    lngDateDiffValue = (long)TS.TotalHours;
-                    break;
-                case DateInterval.Minute:
-                    lngDateDiffValue = (long)TS.TotalMinutes;
-                    break;
-                case DateInterval.Month:
-                    lngDateDiffValue = (long)(TS.Days / 30);
-                    break;
-                case DateInterval.Quarter:
-                    lngDateDiffValue = (long)((TS.Days / 30) / 3);
-                    break;
-                case DateInterval.Second:
-                    lngDateDiffValue = (long)TS.TotalSeconds;
-                    break;
-                case DateInterval.Week:
-                    lngDateDiffValue = (long)(TS.Days / 7);
-                    break;
-                case DateInterval.Year:
-                    lngDateDiffValue = (long)(TS.Days / 365);
-                    break;
-            }
-            return (lngDateDiffValue);
-        }//end of DateDiff
+		public static bool IsNumeric (string theValue)
+		{
+			try {
+				Convert.ToDouble (theValue);
+				return true;
+			} catch {
+				return false;
+			}
+		}
 
-        public static bool IsNumeric(string theValue)
-        {
-            try
-            {
-                Convert.ToDouble(theValue);
-                return true;
-            } 
-            catch 
-            {
-                return false;
-            }
-        }
+		//TODO: sacar esto de acá !!!!!!!!!!!!!!!!!!!! Quitar referencia DBNET tambien
+		public static string consultarFechaActual ()
+		{
+			string fecha;
+			fecha = Model.DB.ejecutarScalar (Model.TipoComando.SP, "fechaActual").ToString ();
+			return fecha;
+		}
 
+		/// <summary>
+		/// Arma la cadena AnioMes, a partir de un año y mes dados.
+		/// formato aaaamm
+		/// </summary>
+		/// <returns></returns>
+		[Obsolete ("Usar string.Format(\"{0:0000}{1:00}, anio, mes\");")]
+		public static string anioMes (int anio, byte mes)
+		{
+			string strMes;
+			if (mes > 9)
+				strMes = mes.ToString ();
+			else
+				strMes = 0 + mes.ToString ();
+			return anio + strMes;
+		}
 
+		/// <summary>
+		/// realiza consulta generica para confirmar la eliminacion de un registro
+		/// Está seguro de eliminar el registro ?
+		/// </summary>
+		/// <returns></returns>
+		[Obsolete ("Usar ConfirmarEliminarRegitro()")]
+		public static bool confirmaEliminarRegistro ()
+		{
+			ConfirmaEliminarRegistro ();
+		}
 
-        public static string consultarFechaActual()
-        {
-            string fecha;
-            fecha = Model.DB.ejecutarScalar(Model.TipoComando.SP, "fechaActual").ToString();
-            return fecha;
-            ///TODO: sacar esto de acá !!!!!!!!!!!!!!!!!!!! Quitar referencia DBNET tambien
-        }
-        
-        /// <summary>
-        /// Arma la cadena AnioMes, a partir de un año y mes dados.
-        /// formato aaaamm
-        /// </summary>
-        /// <returns></returns>
-        public static string anioMes(int anio, byte mes)
-        {
-            string strMes;
-            if (mes > 9)
-                strMes = mes.ToString();
-            else
-                strMes = 0 + mes.ToString();
-            return anio.ToString() + strMes;
-        }
+		/// <summary>
+		/// realiza consulta generica para confirmar la eliminacion de un registro
+		/// Está seguro de eliminar el registro ?
+		/// </summary>
+		/// <returns></returns>
+		public static bool ConfirmaEliminarRegistro ()
+		{
+			DialogResult result = MessageBox.Show ("Está seguro de eliminar el registro ?", "Caption", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			return result == DialogResult.Yes;
+		}
 
-        /// <summary>
-        /// realiza consulta generica para confirmar la eliminacion de un registro
-        /// Está seguro de eliminar el registro ?
-        /// </summary>
-        /// <returns></returns>
-        public static bool confirmaEliminarRegistro()
-        {
-            DialogResult result = MessageBox.Show("Está seguro de eliminar el registro ?", "Caption", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            return result == DialogResult.Yes;
-        }
+		/// <summary>
+		/// Obtiene parte izquierda de una cadena
+		/// </summary>
+		/// <param name="param"></param>
+		/// <param name="length"></param>
+		/// <returns></returns>
+		public static string Left (this string param, int length)
+		{
+			string result = param;
+			//we start at 0 since we want to get the characters starting from the
+			//left and with the specified lenght and assign it to a variable
+			if (param.Length > length)
+				result = param.Substring (0, length);
+			//return the result of the operation
+			return result;
+		}
 
+		/// <summary>
+		/// obtiene parte derecha de una cadena
+		/// </summary>
+		/// <param name="param"></param>
+		/// <param name="length"></param>
+		/// <returns></returns>
+		public static string Right (this string param, int length)
+		{
+			//start at the index based on the lenght of the sting minus
+			//the specified lenght and assign it a variable
+			string result = param.Substring (param.Length - length, length);
+			//return the result of the operation
+			return result;
+		}
 
+		/// <summary>
+		/// obtiene caracteres dada una posicion de inicio y longitud
+		/// </summary>
+		/// <param name="param"></param>
+		/// <param name="startIndex"></param>
+		/// <param name="length"></param>
+		/// <returns></returns>
+		public static string Mid (this string param, int startIndex, int length)
+		{
+			//start at the specified index in the string ang get N number of
+			//characters depending on the lenght and assign it to a variable
+			string result = param.Substring (startIndex, length);
+			//return the result of the operation
+			return result;
+		}
 
-
-        /// <summary>
-        /// Obtiene parte izquierda de una cadena
-        /// </summary>
-        /// <param name="param"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        public static string Left(string param, int length)
-        {
-            string result=param;
-            //we start at 0 since we want to get the characters starting from the
-            //left and with the specified lenght and assign it to a variable
-           if (param.Length>length )
-                result = param.Substring(0, length);
-            //return the result of the operation
-            return result;
-        }
-
-        /// <summary>
-        /// obtiene parte derecha de una cadena
-        /// </summary>
-        /// <param name="param"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        public static string Right(string param, int length)
-        {
-            //start at the index based on the lenght of the sting minus
-            //the specified lenght and assign it a variable
-            string result = param.Substring(param.Length - length, length);
-            //return the result of the operation
-            return result;
-        }
-
-        /// <summary>
-        /// obtiene caracteres dada una posicion de inicio y longitud
-        /// </summary>
-        /// <param name="param"></param>
-        /// <param name="startIndex"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        public static string Mid(string param, int startIndex, int length)
-        {
-            //start at the specified index in the string ang get N number of
-            //characters depending on the lenght and assign it to a variable
-            string result = param.Substring(startIndex, length);
-            //return the result of the operation
-            return result;
-        }
-
-        /// <summary>
-        /// Obtiene caracteres a partir de una posicion dada hasta el final
-        /// </summary>
-        /// <param name="param"></param>
-        /// <param name="startIndex"></param>
-        /// <returns></returns>
-        public static string Mid(string param, int startIndex)
-        {
-            //start at the specified index and return all characters after it
-            //and assign it to a variable
-            string result = param.Substring(startIndex);
-            //return the result of the operation
-            return result;
-        }
-    }
-
-
+		/// <summary>
+		/// Obtiene caracteres a partir de una posicion dada hasta el final
+		/// </summary>
+		/// <param name="param"></param>
+		/// <param name="startIndex"></param>
+		/// <returns></returns>
+		public static string Mid (this string param, int startIndex)
+		{
+			//start at the specified index and return all characters after it
+			//and assign it to a variable
+			string result = param.Substring (startIndex);
+			//return the result of the operation
+			return result;
+		}
+	}
 }
