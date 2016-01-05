2c0b1e5c80560ab87445b804bea56b4a1d57277d
Hernan Vivani <hernan@vivani.com.ar>
2016-01-05 19:43:46 +0200

    adding Log4Net

---
+++ SOffT.Log4Net/MyLog4Net.cs
@@ -0,0 +1,93 @@
+﻿/*  
+    MyLog4Net.cs
+
+    Author:
+    Hernan Vivani <hernan@vivani.com.ar> - http://hvivani.com.ar
+
+    Copyright © SOffT 2010 - http://www.sofft.com.ar
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+using System;
+using System.IO;
+using log4net;
+using log4net.Config;
+
+namespace Log4Net {
+
+    /// <summary>
+    /// Logging utilizando: Log4Net
+    /// Esta clase provee el acceso a 2 objetos para emitir logs: defaultLog y customLog.
+    /// La configuración de los mismos se encuentra en log4net.config (o app.config).
+    /// DefaultLog utiliza un log por defecto pre configurado.
+    /// CustomLog permite recuperar un objeto de log con el nombre de la clase que lo invoca.
+    /// <example><code>Uso: MyLog4Net.Instance.DefaultLog.Info("mensaje de log")</code></example>
+    /// <author> Ramiro </author> </summary>
+    /// <remarks>Singleton class</remarks>
+    public sealed class MyLog4Net { // public final class
+
+        private ILog defaultLog; // se crea al generar el objeto (única instancia)
+
+        /// <summary>Constructor. Crea el log por defecto.</summary>
+        private MyLog4Net() {
+            // lee el archivo de configuración con la definición de los logs.
+            FileInfo configFile = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            if (configFile.Exists) {
+                XmlConfigurator.Configure(configFile);
+                XmlConfigurator.ConfigureAndWatch(configFile);
+            } else {
+                // sin parámetros toma el config de la Aplicación que utiliza el paquete (.dll)
+                // XmlConfigurator.Configure(); // toma de app.config
+
+                // crea una configuración por defecto
+                BasicConfigurator.Configure();
+
+                // TODO: cargar una configuración mínima acorde por programa....
+            }
+
+            // instancia el Log por defecto.
+            this.defaultLog = LogManager.GetLogger("InfoLog"); // this.ToString()
+        }
+
+        public static readonly MyLog4Net Instance = new MyLog4Net();
+
+
+        /// <summary>Devuelve el objeto de log por defecto.</summary>
+        public ILog DefaultLog {
+            get {
+                return this.defaultLog;
+            }
+        }
+
+        /// <summary>
+        /// Devuelve un objeto de log basado en el tipo de la clase que lo invocó.
+        /// </summary>
+        /// <param name="typeOfClass">Type de la clase que lo solicita. Ej: this.GetType()</param>
+        public ILog getCustomLog(Type typeOfClass) {
+            return LogManager.GetLogger(typeOfClass);
+        }
+
+        /// <summary>
+        /// Devuelve un objeto de log basado en el nombre de la clase que lo invocó.
+        /// </summary>
+        /// <param name="nameOfClass">Nombre de la clase que lo solicita.</param>
+        public ILog getCustomLog(string nameOfClass) {
+            return LogManager.GetLogger(nameOfClass);
+        }
+
+    } // End of class
+
+} // End of package
---
+++ SOffT.Log4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,61 @@
+﻿/*  
+    AssemblyInfo.cs
+
+    Author:
+    Hernan Vivani <hernan@vivani.com.ar> - http://hvivani.com.ar
+
+    Copyright © SOffT 2010 - http://www.sofft.com.ar
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Log4Net")]
+[assembly: AssemblyDescription("Clases para logging usando Log4Net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SOffT")]
+[assembly: AssemblyProduct("Log4Net")]
+[assembly: AssemblyCopyright("Copyright SOffT © 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("a42c6b58-7336-486b-b282-7a3ced2746fd")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de versión de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+// mediante el asterisco ('*'), como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("es-AR")]
